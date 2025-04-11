359f9ccada319869ac825ad5aef98401a5b060e9
klajdihoxha1 <klajdi.hoxha@gmail.com>
2025-04-11 11:48:33 +0200

    pruebas con vim

--- tests/suma.js
+++ tests/suma.js
@@ -17,8 +17,23 @@ for(let i= 0 ; i < a; i++){
 //A commit from github
 //2-Este new commit is from my VSC 
 //B-Otro commit from github 
-//Code Insertd from VSC
+//Code Insertd from VSCi
 //C-Otro codigo from github
 //3- Otro codigo from VSC
-
-
+// Klajdi ha metido este comentaorio from vim editor
+// para abir un fichero con vim
+// vim file
+// para edit el fichero pulsa i
+// para guardar y salir, pusla esc , then pulsa :wq ay luego enter, vamos hacerloa hora
+// para salir sin guardar es esc, :q! y luego enter , a q mola ?v
+// cosas con el editor de vim
+// guardo y me salgo
+// // guardo y me salgo
+///////// guardo y me salgo
+/// guardo y me salgo
+/// guardo y me salgo
+/// guardo y me salgo
+/// guardo y me salgo
+/// guardo y me salgo
+/// guardo y me salgo
+//
