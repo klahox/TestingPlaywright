299f68e950c39e4c7cf0d231d723d92064a2a3bc
klajdihoxha1 <klajdi.hoxha@gmail.com>
2025-04-11 10:57:52 +0200

    addin a new fle

---
+++ tests/suma.js
@@ -0,0 +1,17 @@
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+/*
+function suma(a,b){
+    return a+b;
+}
+
+
+function resta(a,b){
+    return a-b;
+}*/
+for(let i= 0 ; i < a; i++){
+    alert(`Loren es un crack ${i}`);
+}
+
+
+
